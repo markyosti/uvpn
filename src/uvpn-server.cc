fb6e66c4f9fa095595fa67cb606ee23be5ddb83a
Mark Moreno <kramonerom@gmail.com>
2012-05-25 16:09:19 -0700

    uvpn-server now uses libyaarg as well.

--- src/uvpn-server.cc
+++ src/uvpn-server.cc
@@ -1,8 +1,37 @@
+// Copyright (c) 2008,2009,2010,2011 Mark Moreno (kramonerom@gmail.com).
+// All rights reserved.
+// 
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+// 
+//    1. Redistributions of source code must retain the above copyright notice,
+//    this list of conditions and the following disclaimer.
+// 
+//    2. Redistributions in binary form must reproduce the above copyright
+//    notice, this list of conditions and the following disclaimer in the
+//    documentation and/or other materials provided with the distribution.
+// 
+// THIS SOFTWARE IS PROVIDED BY Mark Moreno ''AS IS'' AND ANY EXPRESS OR
+// IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+// MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO
+// EVENT SHALL Mark Moreno OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+// INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+// (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+// ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+// THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+// 
+// The views and conclusions contained in the software and documentation are
+// those of the authors and should not be interpreted as representing official
+// policies, either expressed or implied, of Mark Moreno.
+
 #include "base.h"
 
 #include <string>
 #include <memory>
 
+#include "uvpn-server.h"
 #include "dispatcher.h"
 
 #include "tun-tap-server-channel.h"
@@ -15,15 +44,26 @@
 #include "daemon-controller.h"
 #include "daemon-controller-server.h"
 
-// So, the client:
-//   - creates a dispatcher, transport, and transcoder.
-//   - calls authenticate, which starts sending authentication
-//     packets by using the scramble session protector.
-// The server:
-//   - creates a dispatcher, transport, and transcoder.
-//   - waits for new authentication requests to arrive.
+UvpnServer::UvpnServer(ConfigParser* parser)
+    : type_(
+          parser, Option::Default, "type", "t", "server",
+          "Each uvpn runs multiple processes to provide vpn "
+          "connections. Tipically, you will have a 'client' process and "
+          "a 'server' process. By using this option, you can start uvpn "
+          "processes in some custom role. You don't normally need to "
+          "change it, but if you do, remember that you also need to "
+          "pass it to uvpn-ctl."),
+      name_(
+          parser, Option::Default, "name", "n", "default",
+          "If you run multiple uvpn 'server's or multiple uvpn 'client's "
+          "(see the --type option), you need each server or each client "
+          "to have a different name. With this option, you can specify "
+          "the name of the uvpn instance to launch. You don't normally need "
+          "to specify this option, but if you do, remember that you also need "
+          "to pass it to uvpn-ctl.") {
+}
 
-int main(int argc, char** argv) {
+int UvpnServer::Run() {
   Dispatcher dispatcher;
   if (!dispatcher.Init()) {
     LOG_FATAL("could not initialize dispatcher");
@@ -77,4 +117,6 @@ int main(int argc, char** argv) {
   t_udp.Start();
   t_tcp.Start();
   dispatcher.Start();
+
+  return 0;
 }
---
+++ src/uvpn-server.h
@@ -0,0 +1,45 @@
+// Copyright (c) 2008,2009,2010,2011 Mark Moreno (kramonerom@gmail.com).
+// All rights reserved.
+// 
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+// 
+//    1. Redistributions of source code must retain the above copyright notice,
+//    this list of conditions and the following disclaimer.
+// 
+//    2. Redistributions in binary form must reproduce the above copyright
+//    notice, this list of conditions and the following disclaimer in the
+//    documentation and/or other materials provided with the distribution.
+// 
+// THIS SOFTWARE IS PROVIDED BY Mark Moreno ''AS IS'' AND ANY EXPRESS OR
+// IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+// MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO
+// EVENT SHALL Mark Moreno OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+// INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+// (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+// ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+// THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+// 
+// The views and conclusions contained in the software and documentation are
+// those of the authors and should not be interpreted as representing official
+// policies, either expressed or implied, of Mark Moreno.
+
+#ifndef UVPN_SERVER_H
+# define UVPN_SERVER_H
+
+# include "base.h"
+# include "yaarg.h"
+
+class UvpnServer {
+ public:
+  UvpnServer(ConfigParser* parser);
+  int Run();
+
+ private:
+  StringOption type_;
+  StringOption name_;
+};
+
+#endif /* UVPN_SERVER_H */
